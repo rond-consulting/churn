795694570ddc979789b71807c7c307b278cc227e
Hans <hans.weda@rond.nu>
2021-02-10 21:47:53 +0100

    Gathering some links first

---
+++ src/survival_analysis_for_churn.py
@@ -0,0 +1,16 @@
+"""
+Trying survival analysis for churn
+
+Excellent blog: https://towardsdatascience.com/churn-prediction-and-prevention-in-python-2d454e5fd9a5
+--> let's try to replicate that!
+
+This is where you can find the data: https://www.kaggle.com/bandiatindra/telecom-churn-prediction
+
+Lifelines API documentation: https://lifelines.readthedocs.io/en/latest/References.html
+
+More simple blog, but not bad either: https://towardsdatascience.com/survival-analysis-to-understand-customer-retention-e3724f3f7ea2
+
+Paper on survival analysis for churn: http://ceur-ws.org/Vol-2577/paper5.pdf
+
+
+"""
