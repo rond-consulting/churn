8c4f9e0fd93979af6fa176d736031c34069b2f7e
Hans <hans.weda@rond.nu>
2021-02-12 16:44:24 +0100

    Refactoring loading from Kaggle: the loading from kaggle is put into data_utils.py

--- src/survival_analysis_for_churn.py
+++ src/survival_analysis_for_churn.py
@@ -30,39 +30,7 @@ from sklearn.model_selection import train_test_split
 
 from src.visualization.prediction_plot_utils import plot_result
 from src.models.model_utils import remove_collinear_variables
-
-load_dotenv(find_dotenv())
-
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-
-DATA_DIR = os.path.join("..", "data", "raw")
-
-
-def load_from_kaggle(owner: str, dataset_name: str) -> pd.DataFrame:
-    """
-    This function downloads data from kaggle into DATA_DIR and returns the first file (alphabetically).
-    Following directions on https://medium.com/analytics-vidhya/fetch-data-from-kaggle-with-python-9154a4c610e3
-    The documentation of the kaggle api can be found here: https://github.com/Kaggle/kaggle-api/tree/master/kaggle
-    :param owner: the owner of the dataset on Kaggle
-    :param dataset_name: the name of the dataset
-    :return: the alphabetically first file from the downloaded dataset
-    """
-    # make sure raw data directory exists
-    target_directory = os.path.join(DATA_DIR, dataset_name)
-    os.makedirs(target_directory, exist_ok=True)
-
-    # connect to kaggle
-    api = KaggleApi()
-    api.authenticate()
-
-    # download if it doesn't exist already and unzip
-    api.dataset_download_files(dataset=f"{owner}/{dataset_name}", path=target_directory, unzip=True)
-
-    # read the first file and return
-    df = pd.read_csv(os.path.join(target_directory, os.listdir(target_directory)[0]))
-
-    return df
+from src.data.data_utils import load_from_kaggle
 
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
